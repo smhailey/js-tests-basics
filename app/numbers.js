46b44f7f096854a7fde1ff2a1ea341e33ee3d54a
smhailey <smhailey@msn.com>
2019-06-11 15:48:47 -0600

    4 answers correct

--- app/numbers.js
+++ app/numbers.js
@@ -1,11 +1,11 @@
 // Write a function that uses the addition operator to add two numbers together
 function add(x, y) {
-
+  return (x + y)
 }
 
 // Write a function that returns the Cubed value of x
 function cubed(x) {
-
+  return (x * x * x)
 }
 
 // Write a function that will divide two numbers, and will format the returned value
--- app/strings.js
+++ app/strings.js
@@ -5,6 +5,7 @@ function letterAtPosition(str, position) {
 
 // Write a function that will join two strings with an optional seperating character 
 function concat(str1, str2, seperator = " ") {
+
 }
 
 // Write a function that will capitalize the letter at a given position and return it

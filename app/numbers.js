2634c500666b8c9ae0149227bf68107ee7d64a15
smhailey <smhailey@msn.com>
2019-06-11 16:45:23 -0600

    8 answers correct

--- app/numbers.js
+++ app/numbers.js
@@ -11,12 +11,14 @@ function cubed(x) {
 // Write a function that will divide two numbers, and will format the returned value
 // as a whole number. (No Decimals)
 function divideWholey(x, y) {
-
+  return Math.floor(x / y)
 }
+console.log(divideWholey(7, 3))
 
 // Write a function that takes in a number and determines if it is even. 
 // It should return a boolean value - true if it is even, false if it is odd.
 function isEven(num) {
+
 }
 
 
